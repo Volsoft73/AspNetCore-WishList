be48a4accd29b62c65113bf5e4e97ffba587a6f6
volsoft <sandro.lomazzo@hotmail.it>
2019-01-09 17:29:17 +0100

    Completetd the module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _contex;
+
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _contex = context;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            var model = _contex.Items.ToList();
+
+            return View("Index", model);
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _contex.Items.Add(item);
+
+            _contex.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var item = _contex.Items.FirstOrDefault(i => i.Id == id);
+
+            _contex.Items.Remove(item);
+
+            _contex.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
